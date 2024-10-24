c1374bbc95cc232eea63f35738bf90855278969d
Ariel-Mamani <arielmamani764@gmail.com>
2024-10-24 18:49:59 -0300

    tablas y clase usuario

---
+++ Modelo/Usuario.php
@@ -0,0 +1,158 @@
+<?php
+class Usuario{
+    private $idUsuario;
+    private $nombre;
+    private $password;
+    private $email;
+    private $mensajeoperacion;
+
+    public function __construct()
+    {
+        $this->idUsuario = "";
+        $this->nombre = "";
+        $this->password = "";
+        $this->email = "";
+        $this->mensajeoperacion = "";
+    }
+
+    public function setear($idUsuario, $nombre, $password, $email)    {
+        $this->setIdUsuario($idUsuario);
+        $this->setNombre($nombre);
+        $this->setPassword($password);
+        $this->setEmail($email);
+    }
+    // Metodo get y set ID
+    public function getIdUsuario(){
+        return $this->idUsuario;
+    }
+    public function setIdUsuario($valor){
+        $this->idUsuario = $valor;
+    }
+    
+    // Metodo get y set NOMBRE
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function setNombre($valor){
+        $this->nombre = $valor;
+    }
+    
+    // Metodo get y set PASSWORD
+    public function getPassword(){
+        return $this->password;
+    }
+    public function setPassword($valor){
+        $this->password = $valor;
+    }
+        
+    // Metodo get y set EMAIL
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setEmail($valor){
+        $this->email = $valor;
+    }
+
+    // Metodo get y set MENSAJE ERROR
+    public function getmensajeoperacion(){
+        return $this->mensajeoperacion;
+    }
+    public function setmensajeoperacion($valor){
+        $this->mensajeoperacion = $valor; 
+    }
+
+    public function buscar($idUsuario){
+        $base = new BaseDatos();
+        $exito = false;
+        $sql = "Select * from usuario where idusuario =" . $idUsuario;
+        if($base ->Iniciar()){
+            if($base->Ejecutar($sql)){
+                if($row = $base->Registro()){
+                    $this -> setIdUsuario($idUsuario);
+                    $this -> setNombre($row['usnombre']);
+                    $this -> setPassword($row['uspass']);
+                    $this -> setEmail($row['usemail']);
+                    $exito = true;
+                }
+            }
+        }
+        return $exito;
+    }
+
+    public function insertar(){
+        $resp = false;
+        $base  =  new BaseDatos();
+        $sql  =  "INSERT INTO usuario(idusuario, usnombre, uspass, usemail) VALUES('"
+        .$this->getIdUsuario()."', '"
+        .$this->getNombre()."', '"
+        .$this->getPassword()."', '"
+        .$this->getEmail()."');";
+        if ($base->Iniciar()) {
+            if($base->Ejecutar($sql)){
+                $resp = true;
+            }else{
+                $this->setmensajeoperacion("Usuario->insertar: ".$base->getError());
+            }
+        }else{
+            $this->setmensajeoperacion("Usuario->insertar: ".$base->getError());
+        }
+        return $resp;
+    }
+
+    public function modificar(){
+        $resp = false;
+        $base = new BaseDatos();
+        $sql = "UPDATE usuario SET 
+        usnombre = '".$this->getNombre()."', 
+        uspass = '".$this->getPassword()."', 
+        usemail = '".$this->getEmail()."' WHERE idusuario = '".$this->getIdUsuario()."'";
+        if ($base->Iniciar()) {
+            if($base->Ejecutar($sql)){
+                $resp = true;
+            }else{
+                $this->setmensajeoperacion("Tabla->modificar: ".$base->getError());
+            }
+        }else{
+            $this->setmensajeoperacion("Tabla->modificar: ".$base->getError());
+        }
+        return $resp;
+    }
+
+    public function eliminar(){
+        $resp = false;
+        $base = new BaseDatos();
+        $sql = "DELETE FROM usuario WHERE idusuario = '".$this->getIdUsuario()."'";
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql)) {
+                return true;
+            }else{
+                $this->setmensajeoperacion("Tabla->eliminar: ".$base->getError());
+            }
+        }else{
+            $this->setmensajeoperacion("Tabla->eliminar: ".$base->getError());
+        }
+        return $resp;
+    }
+
+    public static function listar($parametro=""){
+        $arreglo = array();
+        $base = new BaseDatos();
+        $sql = "SELECT * FROM usuario ";
+        if ($parametro != "") {
+            $sql .= " WHERE " .$parametro;
+        }
+        $res = $base->Ejecutar($sql);
+        if($res > -1){
+            if($res > 0){
+                while ($row = $base->Registro()){
+                    $obj = new Usuario();
+                    $obj->setear($row['idusuario'], $row['usnombre'], $row['uspass'], $row['usemail']);
+                    array_push($arreglo, $obj);
+                }
+            }
+        }else{
+            self::setmensajeoperacion("Usuario->listar: ".$base->getError());
+        }
+        return $arreglo;
+    }
+}
