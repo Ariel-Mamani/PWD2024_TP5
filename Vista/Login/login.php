18b1776c452f8ebbbe9a9a8e5bf980bb9b235d61
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-10-31 15:22:43 -0300

    Estilos Login y Registro, sólo falta un detalles

--- Vista/Login/login.php
+++ Vista/Login/login.php
@@ -4,44 +4,78 @@ $titulo = "TP 5 - Login ";
 include_once '../Estructura/header.php';
 $objSession = new Session();
 ?>
-<div class="divtitulo">
-    <h1 class='text-center mb-4 text-white'><?php echo $titulo;?></h1>
-</div>
-<?php 
-// El mensaje viene del script procesar_login.php y verificarLogin.php
-// Mi idea es mostrar el mensaje de que el usuario se registro
-// Verifica si hay un mensaje en la sesión y lo muestra
-if (isset($_SESSION['mensaje']) && $_SESSION['mensaje'] != '') {
-    echo "<h1 class='bg-dark text-light p-2'>{$_SESSION['mensaje']}</h1>";
-    // Limpia el mensaje para que no vuelva a mostrarse en el próximo acceso
-    unset($_SESSION['mensaje']);
-}
-?>
 
-<div class="divform rounded p-4 shadow">
-<!-- Formulario Login -->
-    <form id="form" name="form" action="verificarLogin.php" method="post" class="full-height p-5 needs-validation" novalidate>
-        <!-- Nombre usuario  -->
-        <div class="input-group mb-4">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person" style="font-size: 22px; color: black;"></i></span>
-            <input type="text" name="usnombre" id="usnombre" class="form-control" placeholder="Usuario" required><br><br>
-            <!-- Mensajes aprobado y error -->
-            <div class="valid-feedback">Ok!</div>
-            <div class="invalid-feedback">El nombre de usuario solo debe contener letras sin números ni símbolos</div>
-        </div>
-        <!-- Contraseña -->
-        <div class="input-group mb-4">
-            <span class="input-group-text" id="basic-addon2"><i class="bi bi-lock-fill" style="font-size: 22px;"></i></span>
-            <input id="clave" name="clave" type="password" class="form-control" placeholder="Contraseña" required onblur="return convertirClaveMD5()">
-            <input type="hidden" name="uspass" id="uspass">
-            <!-- Mensajes aprobado y error -->
-            <div class="valid-feedback">Ok!</div>
-            <div class="invalid-feedback">Valor de contraseña incorrecto</div>
-        </div>
-        
-        <input type="submit" value="Enviar" class="btn btn-primary mt-5" >
-    </form>
-</div>
+<section>
+
+    <?php 
+    // El mensaje viene del script procesar_login.php y verificarLogin.php
+    // Mi idea es mostrar el mensaje de que el usuario se registro
+    // Verifica si hay un mensaje en la sesión y lo muestra
+    if (isset($_SESSION['mensaje']) && $_SESSION['mensaje'] != '') {
+        echo "<h1 class='bg-dark text-light p-2'>{$_SESSION['mensaje']}</h1>";
+        // Limpia el mensaje para que no vuelva a mostrarse en el próximo acceso
+        unset($_SESSION['mensaje']);
+    }
+    ?>
+
+    <div class="login-box">
+    <!-- Formulario Login -->
+        <form id="form" name="form" action="verificarLogin.php" method="post" class="full-height p-5 needs-validation" novalidate>
+
+            <!-- Titulo -->
+            <h2>Login</h2>
+
+            <!-- Nombre usuario  -->
+            <div class="input-group mb-4 input-box">
+                <!-- Icono -->
+                <span class="icon" id="basic-addon1">
+                    <ion-icon name="person"></ion-icon>
+                </span>
+
+                <input type="text" name="usnombre" id="usnombre" class="form-control" required>
+                <label>Ususario</label>
+
+                <!-- Mensajes aprobado y error -->
+                <div class="valid-feedback">Ok!</div>
+                <div class="invalid-feedback">El nombre de usuario solo debe contener letras sin números ni símbolos</div>
+            </div>
+
+            <!-- Contraseña -->
+            <div class="input-group mb-4 input-box">
+                <!-- Icono -->
+                <span class="icon" id="basic-addon2">
+                    <ion-icon name="lock-closed"></ion-icon>
+                </span>
+
+                <input id="clave" name="clave" type="password" class="form-control" required onblur="return convertirClaveMD5()">
+                <input type="hidden" name="uspass" id="uspass">
+                <label>Password</label>
+                
+                <!-- Mensajes aprobado y error -->
+                <div class="valid-feedback">Ok!</div>
+                <div class="invalid-feedback">Valor de contraseña incorrecto</div>
+            </div>
+            
+            <!-- Recuerdame y olvide contrasenia -->
+            <div class="remember-forgot">
+                <label><input type="checkbox">Recuerdame</label>
+                <a href="#">Olvido su contrase&ntilde;a?</a>
+            </div>
+
+            <!-- Boton -->
+            <button type="submit">Login</button>
+
+            <!-- Link para registrarse -->
+            <div class="register-link">
+                <p>No tiene cuenta? <a href="registro.php">Registrarse</a></p>
+            </div>
+        </form>
+    </div>
+</section>
+
+<!-- Iconos -->
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 <!-- BOOTSTRAP con las validaciones de los campos -->
 <script type="text/javascript" src="../Js/validacionTP5.js"></script>
@@ -56,6 +90,7 @@ if (isset($_SESSION['mensaje']) && $_SESSION['mensaje'] != '') {
         validarUsuario(this);
     });
 </script>
+
 <!-- Footer -->
 <?php include_once '../Estructura/footer.php'; ?>
 
--- Vista/Login/registro.php
+++ Vista/Login/registro.php
@@ -2,56 +2,85 @@
 $titulo = "TP 5 - Registrar ";
 include_once '../Estructura/header.php';
 ?>
-<div class="divtitulo">
-    <h1 class='text-center mb-4 text-white'><?php echo $titulo;?></h1>
-</div>
-<?php
-// El mensaje viene del script verificarLogin.php
-// Se mostrara un mensaje ya que el usuario quiso entrar al login sin haberse registrado antes
-// Verifica si hay un mensaje en la sesión y lo muestra
-if (isset($_SESSION['mensaje']) && $_SESSION['mensaje'] != '') {
-    echo "<h1 class='bg-dark text-light p-2'>{$_SESSION['mensaje']}</h1>";
-    // Limpia el mensaje para que no vuelva a mostrarse en el próximo acceso
-    unset($_SESSION['mensaje']);
-}
-?>
-<body>
 
-<div class="divform rounded p-4 shadow">
-<!-- Formulario de Registro -->
-    <form id="form" name="form" action="procesar_registro.php" method="get" class="needs-validation" novalidate>
-        <!-- Nombre usuario  -->
-        <div class="input-group mb-4">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person" style="font-size: 22px; color: black;"></i></span>
-            <input type="text" name="usnombre" id="usnombre" class="form-control" placeholder="Usuario" required><br><br>
-            <!-- Mensajes aprobado y error -->
-            <div class="valid-feedback">Ok!</div>
-            <div class="invalid-feedback">El nombre de usuario solo debe contener letras sin números ni símbolos</div>
-        </div>
-        <!-- Contraseña -->
-        <div class="input-group mb-4">
-            <span class="input-group-text" id="basic-addon2"><i class="bi bi-lock-fill" style="font-size: 22px;"></i></span>
-            <input id="clave" name="clave" type="password" class="form-control" placeholder="Contraseña" required onblur="return convertirClaveMD5()">
-            <input type="hidden" name="uspass" id="uspass">
-            <!-- investigar sobre password_hash() -->
-            <!-- Mensajes aprobado y error -->
-            <div class="valid-feedback">Ok!</div>
-            <div class="invalid-feedback">Valor de contraseña incorrecto</div>
-        </div>
-        <!-- Email -->
-        <div class="input-group">
-            <span class="input-group-text">usuario123@example.com</span>
-            <input type="email" name="usmail" id="usmail" class="form-control" required><br><br>
-            <!-- Mensajes aprobado y error -->
-            <div class="valid-feedback">Ok!</div>
-            <div class="invalid-feedback">Email invalida</div>
-        </div>
-        
-        <input type="submit" value="Registrar"  class="btn btn-primary mt-5">
-        </div>
-    </form>
-</div>
+    <?php
+    // El mensaje viene del script verificarLogin.php
+    // Se mostrara un mensaje ya que el usuario quiso entrar al login sin haberse registrado antes
+    // Verifica si hay un mensaje en la sesión y lo muestra
+    if (isset($_SESSION['mensaje']) && $_SESSION['mensaje'] != '') {
+        echo "<h1 class='bg-dark text-light p-2'>{$_SESSION['mensaje']}</h1>";
+        // Limpia el mensaje para que no vuelva a mostrarse en el próximo acceso
+        unset($_SESSION['mensaje']);
+    }
+    ?>
+<section>
+
+    <body>
+
+    <div class="login-box">
+    <!-- Formulario de Registro -->
+        <form id="form" name="form" action="procesar_registro.php" method="get" class="needs-validation" novalidate>
+
+        <!-- Titulo -->
+        <h2>Registro</h2>
+
+            <!-- Nombre usuario  -->
+            <div class="input-group mb-4 input-box">
+                <!-- Icono -->
+                <span class="icon" id="basic-addon1">
+                    <ion-icon name="person"></ion-icon>
+                </span>
+
+                <input type="text" name="usnombre" id="usnombre" class="form-control" required>
+                <label>Usuario</label>
+                <!-- Mensajes aprobado y error -->
+                <div class="valid-feedback">Ok!</div>
+                <div class="invalid-feedback">El nombre de usuario solo debe contener letras sin números ni símbolos</div>
+            </div>
+
+            <!-- Contraseña -->
+            <div class="input-group mb-4 input-box">
+                <!-- Icono -->
+                <span class="icon" id="basic-addon2">
+                    <ion-icon name="lock-closed"></ion-icon>
+                </span>
+
+                <input id="clave" name="clave" type="password" class="form-control" required onblur="return convertirClaveMD5()">
+                <input type="hidden" name="uspass" id="uspass">
+                <label>Password</label>
+                <!-- investigar sobre password_hash() -->
+                
+                <!-- Mensajes aprobado y error -->
+                <div class="valid-feedback">Ok!</div>
+                <div class="invalid-feedback">Valor de contraseña incorrecto</div>
+            </div>
+
+            <!-- Email -->
+            <div class="input-group input-box">
+                <!-- Icono -->
+                <span class="icon">
+                    <ion-icon name="mail"></ion-icon>
+                </span>
+
+                <input type="email" name="usmail" id="usmail" class="form-control" required>
+                <label>usuario123@example.com</label>
+
+                <!-- Mensajes aprobado y error -->
+                <div class="valid-feedback">Ok!</div>
+                <div class="invalid-feedback">Email invalida</div>
+            </div>
+            
+            <input type="submit" value="Registrar"  class="btn btn-primary mt-5">
+            </div>
+        </form>
+    </div>
 </body>
+</section>
+
+<!-- Iconos -->
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 <!-- BOOTSTRAP con las validaciones de los campos -->
 <script type="text/javascript" src="../Js/validacionTP5.js"></script>
 <script>
