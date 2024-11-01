f2fa217d822c5722d48aca4d25d330485f117d92
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-11-01 14:12:41 -0300

    Detalle

--- Vista/Inicio/principal.php
+++ Vista/Inicio/principal.php
@@ -6,6 +6,7 @@ include_once '../Estructura/header.php';
     <div class="divtitulo">
         <h1>Programaci&oacute;n Web Din&aacute;mica</h1>
         <h1>Grupo 5 PWD 2024</h1>
+        <br>
     </div>
     <div class="portada">
         <img src="../Imagenes/Portada 2.png" alt="Imagen de Portada" >
