ed0f42da7f4b19543e3b5fe7a5f204219d0b05fa
Ariel-Mamani <arielmamani764@gmail.com>
2024-10-31 21:31:00 -0300

    relacion usuarioRol

--- Vista/Login/procesar_registro.php
+++ Vista/Login/procesar_registro.php
@@ -5,20 +5,22 @@ include_once '../Estructura/header.php';
 echo "<div class='divtitulo'><h1>{$titulo}</h1>";
 $objSession = new Session();
 $objAbmUsuario = new AbmUsuario();
+$objAbmUsuarioRol = new AbmUsuarioRol();
 if(!empty(data_submitted())){
     $recibido = data_submitted();
     $param = Array();
     $param['usnombre']  = $recibido['usnombre'];
     $param['usmail']    = $recibido['usmail'];  
     $listaUsuario = $objAbmUsuario->buscar($param);
-    if(empty($listaUsuario)){ // ¿verificamos con nombre de usuario y mail?
-        if($objAbmUsuario->alta($recibido)){
-            $mensaje = $objSession->getMensaje(true);
-            // Se REDIRIGE al formulario de login si el registro fue exitoso
-            echo "<h1>SI</h1>";
-            header("Location: login.php");
-            exit();
+    if($objAbmUsuario->alta($recibido)) {
+        $usuario = $objAbmUsuario->buscar($param);
+        if (!empty($usuario)) {
+            $usuarioRol = ['idusuario' => $usuario[0]->getidusuario(), 'idrol' => 2];
+            $objAbmUsuarioRol->alta($usuarioRol);
         }
+        $mensaje = $objSession->getMensaje(true);
+        header("Location: login.php");
+        exit();
     }else{
         $mensaje = $objSession->getMensaje();
         echo "<h1>No</h1>";
