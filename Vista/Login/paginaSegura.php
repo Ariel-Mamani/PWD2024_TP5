ab1e61699784c0e5d402d250e58cd445f11be9c1
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-11-01 15:35:14 -0300

    Estilos editar contraseña

--- Vista/Login/paginaSegura.php
+++ Vista/Login/paginaSegura.php
@@ -1,23 +1,31 @@
 <?php
-$titulo = "TP 5 - Login ";
+$titulo = "TP 5 - Pagina Segura ";
 
 include_once '../Estructura/headerSeguro.php';
 ?>
-<div class="divtitulo">
-    <h1 class='text-center mb-4 text-white'><?php echo $titulo;?></h1>
-    <h1>Pagina Segura</h1>
 
-    <?php  
-    /* $enlace_actual = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
-        echo $enlace_actual;
-        var_dump(parse_url($enlace_actual, PHP_URL_PATH));*/
-    ?>
-</div>
-<div class="bg-light  m-3">
-    <a class="btn btn-info" role="button" href="<?php echo $VISTA?>/Usuarios/editarPass.php?accion=editarPass"><i class="bi bi-pencil"></i>Editar Contraseña</a> 
-</div>
-<div>
-    <a class="btn btn-warning m-3" role="button" href="<?php echo $VISTA?>/UsuarioRol/editarRol.php?accion=editarRol"><i class="bi bi-pencil"></i>Editar Rol</a> 
-</div>
+<section>
+    <div class="divtitulo">
+        <!--<h1 class='text-center mb-4 text-white'><?php //echo $titulo;?></h1>-->
+        
+        <?php  
+        /* $enlace_actual = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+            echo $enlace_actual;
+            var_dump(parse_url($enlace_actual, PHP_URL_PATH));*/
+        ?>
+    </div>
+
+    <!-- Titulo -->
+    <h1>P&aacute;gina Segura</h1>
+
+    <div class="bg-light  m-3">
+        <a class="btn btn-info" role="button" href="<?php echo $VISTA?>/Usuarios/editarPass.php?accion=editarPass"><i class="bi bi-pencil"></i>Editar Contraseña</a> 
+    </div>
+
+    <div>
+        <a class="btn btn-warning m-3" role="button" href="<?php echo $VISTA?>/UsuarioRol/editarRol.php?accion=editarRol"><i class="bi bi-pencil"></i>Editar Rol</a> 
+    </div>
+</section>
+
 <!-- Footer -->
 <?php include_once '../Estructura/footer.php'; ?>
--- Vista/Usuarios/editarPass.php
+++ Vista/Usuarios/editarPass.php
@@ -6,30 +6,48 @@
     
     $obj =$objSession->getUsuario();
 
-?>	
-<form method="post" action="accion.php" class="bg-success">
-    <input id="idusuario" name ="idusuario" type="hidden" value="<?php echo ($obj !=null) ? $obj->getidusuario() : "-1"?>" readonly required>
-    <input id="accion" name ="accion" value="<?php echo ($datos['accion'] !=null) ? $datos['accion'] : "nose"?>" type="hidden">
-    <div class="row mb-12">
-        <div class="col-sm-12 ">
-            <div class="form-group has-feedback">
-                            <!-- Contraseña -->
-        <div class="input-group mb-4">
-            <span class="input-group-text" id="basic-addon2"><i class="bi bi-lock-fill" style="font-size: 22px;"></i></span>
-            <input id="clave" name="clave" type="password" class="form-control" placeholder="Contraseña" required onblur="return convertirClaveMD5()">
-            <input type="hidden" name="uspass" id="uspass">
-            <!-- Mensajes aprobado y error -->
-            <div class="valid-feedback">Ok!</div>
-            <div class="invalid-feedback">Valor de contraseña incorrecto</div>
-        </div>
-            </div>
-        </div>
-    </div>
-	
-	<input type="submit" class="btn btn-primary btn-block  m-2" value="<?php echo ($datos['accion'] !=null) ? $datos['accion'] : "nose"?>">
-</form>
-<a href="index.php" class="btn bg-warning m-2">Volver</a>
+?>
 
+<section>
+    <!--Titulo -->
+    <h1>Editar contrase&ntilde;a</h1>
+    <br>
+    
+    <!-- Editar rol Box -->
+    <div class="editar-rol-box">
+        <!-- Curva -->
+        <div class="curva-login" id="curva-login"></div>
+
+        <form method="post" action="accion.php" class="full-height p-5 needs-validation">
+            <input id="idusuario" name ="idusuario" type="hidden" value="<?php echo ($obj !=null) ? $obj->getidusuario() : "-1"?>" readonly required>
+            <input id="accion" name ="accion" value="<?php echo ($datos['accion'] !=null) ? $datos['accion'] : "nose"?>" type="hidden">
+            <div class="row mb-12">
+                <div class="col-sm-12 ">
+                    <div class="form-group has-feedback">
+
+                        <!-- Contraseña -->
+                        <div class="input-group mb-4 input-box">
+                            <!-- Icono -->
+                            <span class="icon" id="basic-addon2">
+                                <i class="bi bi-lock-fill" style="font-size: 22px;"></i>
+                            </span>
+                            <input id="clave" name="clave" type="password" class="form-control" required onblur="return convertirClaveMD5()">
+                            <input type="hidden" name="uspass" id="uspass">
+                            <label>Contrase&ntilde;a</label>
+
+                            <!-- Mensajes aprobado y error -->
+                            <div class="valid-feedback">Ok!</div>
+                            <div class="invalid-feedback">Valor de contraseña incorrecto</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <input type="submit" class="btn-editar-pass btn-primary btn-block  m-2" value="<?php echo ($datos['accion'] !=null) ? $datos['accion'] : "nose"?>">
+        </form>
+    </div>
+    <a href="index.php" class="btn-volver m-2">Volver</a>
+</section>
 <!-- BOOTSTRAP con las validaciones de los campos -->
 <script type="text/javascript" src="../Js/validacionTP5.js"></script>
 <script>
