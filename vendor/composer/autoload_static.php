3f383ecd25924ea23c475fc0e883abfe2e6eb104
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-11-13 18:49:40 -0300

    Instalacion phpmailer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit889f831e09da48cb8affe8b66f071284
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +27,8 @@ class ComposerStaticInit889f831e09da48cb8affe8b66f071284
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit889f831e09da48cb8affe8b66f071284::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit889f831e09da48cb8affe8b66f071284::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit889f831e09da48cb8affe8b66f071284::$classMap;
 
         }, null, ClassLoader::class);
