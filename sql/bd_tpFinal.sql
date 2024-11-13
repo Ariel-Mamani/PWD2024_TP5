c1c98fa13b02a1b9d9cbb3dbf221515c4527943d
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2024-11-13 19:13:32 -0300

    13-11 19h
    Actualizado BDD

--- sql/bd_tpFinal.sql
+++ sql/bd_tpFinal.sql
@@ -10,7 +10,7 @@ CREATE TABLE menu (
     menombre VARCHAR(50) NOT NULL,
     medescripcion VARCHAR(124),
     idpadre BIGINT(20),
-    medeshabilitado TIMESTAMP
+    medeshabilitado TIMESTAMP NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Tabla Rol
@@ -18,6 +18,16 @@ CREATE TABLE rol (
     idrol BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     rodescripcion VARCHAR(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO rol (idrol, rodescripcion) VALUES 
+    (1, 'superadmin'),
+    (2, 'admin'),
+    (3, 'deposito'),
+    (4, 'cliente');
+
+-- --------------------------------------------------------
+-- ------------------
+-- --------------------------------------------------------
 
 -- Tabla Menu Rol
 CREATE TABLE menurol (
@@ -28,16 +38,44 @@ CREATE TABLE menurol (
     FOREIGN KEY (idrol) REFERENCES rol(idrol)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO menurol (idmenu, idrol) VALUES 
+    (1, 1),
+    (2, 1),
+    (3, 1),
+    (4, 1),
+    (1, 2),
+    (2, 2),
+    (3, 2),
+    (4, 2),
+    (1, 3),
+    (2, 3),
+    (3, 3),
+    (4, 3),
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4);    
+
+-- --------------------------------------------------------
+-- ------------------
+-- --------------------------------------------------------
+
 -- Tabla Usuario
 
 CREATE TABLE usuario (
     idusuario BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     usnombre VARCHAR(50),
-    uspass INT(11),
+    uspass VARCHAR(50),
     usmail VARCHAR(50),
-    usdeshabilitado TIMESTAMP
+    usdeshabilitado TIMESTAMP NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO usuario (idusuario, usnombre, uspass, usmail, usdeshabilitado) VALUES
+    (1, 'Jose', '', 'jose@jose.com', null);
+
+-- --------------------------------------------------------
+-- ------------------
+-- --------------------------------------------------------
 -- Tabla Usuario Rol
 CREATE TABLE usuariorol (
     idusuario BIGINT(20) NOT NULL,
@@ -62,7 +100,8 @@ CREATE TABLE producto (
     prodetalle VARCHAR(512),
     proprecio INT(9),
     procantstock INT(11),
-    proimagen VARCHAR(50)
+    proimagen VARCHAR(50),
+    usdeshabilitado TIMESTAMP NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Tabla Compra Item
@@ -81,6 +120,17 @@ CREATE TABLE compraestadotipo (
     cetdescripcion VARCHAR(50),
     cetdetalle VARCHAR(256)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `compraestadotipo`
+--
+
+INSERT INTO `compraestadotipo` (`idcompraestadotipo`, `cetdescripcion`, `cetdetalle`) VALUES
+(1, 'iniciada', 'cuando el usuario : cliente inicia la compra de uno o mas productos del carrito'),
+(2, 'aceptada', 'cuando el usuario administrador da ingreso a uno de las compras en estado = 1 '),
+(3, 'enviada', 'cuando el usuario administrador envia a uno de las compras en estado =2 '),
+(4, 'cancelada', 'un usuario administrador podra cancelar una compra en cualquier estado y un usuario cliente solo en estado=1 ');
+
 
 -- Tabla Compra Estado
 CREATE TABLE compraestado (
