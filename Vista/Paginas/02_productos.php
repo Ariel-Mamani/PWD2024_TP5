68e56c0b2b7dfa5feeff5393a9df5bcbe705a6b6
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-11-19 18:50:51 -0300

    Detalle
    Saco .png de 02_preductos.php en línea 66

--- Vista/Paginas/02_productos.php
+++ Vista/Paginas/02_productos.php
@@ -63,7 +63,7 @@ include_once '../Estructura/header.php';
                         '<p>' +  producto.pronombre + '</p>' +
                         '<p>Precio: $' + producto.proprecio + '</p>' +
                         '<p>Stock: ' + producto.procantstock + '</p>' + 
-                        '<img class="grid-img" src="../../Archivos/Productos/'+producto.proimagen+ '.png" alt="' + producto.pronombre + '" stlye="width:50%; heigth:auto">' +
+                        '<img class="grid-img" src="../../Archivos/Productos/'+producto.proimagen+ '" alt="' + producto.pronombre + '" stlye="width:50%; heigth:auto">' +
                         '<button tpye="submit" class="btn-compra">Añadir al carro</button>'
                     );
 
