a9f29ebdb18d6e3d14223324059f3adec623009a
Ariel-Mamani <arielmamani764@gmail.com>
2024-11-20 09:34:12 -0300

    cambios

--- Vista/js/funciones.js
+++ Vista/js/funciones.js
@@ -1,6 +1,6 @@
 
 // Funcion para procesar y agregar al carrito
-/*
+
 $(document).ready(function (){
     //Detecta el clic del botón con la clase agregar-carrito
     $('.agregar-carrito').on('click', function (){
@@ -35,76 +35,76 @@ $(document).ready(function (){
         }
     });
 });
-*/
 
-$(document).ready(function () {
-    //Eliminar cualquier evento previo antes de vincular
-    $('.agregar-carrito').off('click').on('click', function (){
-        var idArt = $(this).data('id');
-        var nombre = $(this).data('nombre');
-        var precioVenta = $(this).data('precio');
-        var stock = $(this).data('stock');
 
-        if (stock > 0)
-        {
-            $.ajax({
-                url: '../Carrito/agregarCarrito.php',
-                method: 'POST',
-                data: {
-                    idArt: idArt,
-                    nombre: nombre,
-                    cantidad: 1,
-                    stock: stock,
-                    precioVenta: precioVenta
-                },
-                success: function (response){
-                    showNotification('Artículo agregado al carrito!', 'success');
-                },
-                error: function (){
-                    showNotification('Hubo un error al agregar el artículo al carrito.', 'error');
-                }
-            });
-        }else{
-            showNotification('Este artículo no tiene stock suficiente.', 'error');
-        }
-    });
-});
+// $(document).ready(function () {
+//     //Eliminar cualquier evento previo antes de vincular
+//     $('.agregar-carrito').off('click').on('click', function (){
+//         var idArt = $(this).data('id');
+//         var nombre = $(this).data('nombre');
+//         var precioVenta = $(this).data('precio');
+//         var stock = $(this).data('stock');
 
-function showNotification(message, type){
-    //Eliminar notificaciones existentes antes de crear una nueva
-    $('#notification-container .notification').remove();
+//         if (stock > 0)
+//         {
+//             $.ajax({
+//                 url: '../Carrito/agregarCarrito.php',
+//                 method: 'POST',
+//                 data: {
+//                     idArt: idArt,
+//                     nombre: nombre,
+//                     cantidad: 1,
+//                     stock: stock,
+//                     precioVenta: precioVenta
+//                 },
+//                 success: function (response){
+//                     showNotification('Artículo agregado al carrito!', 'success');
+//                 },
+//                 error: function (){
+//                     showNotification('Hubo un error al agregar el artículo al carrito.', 'error');
+//                 }
+//             });
+//         }else{
+//             showNotification('Este artículo no tiene stock suficiente.', 'error');
+//         }
+//     });
+// });
 
-    var notification = $(`
-        <div class="notification ${type}">
-            <span>${message}</span>
-            <button class="close-btn">&times;</button>
-        </div>
-    `);
+// function showNotification(message, type){
+//     //Eliminar notificaciones existentes antes de crear una nueva
+//     $('#notification-container .notification').remove();
 
-    $('#notification-container').append(notification);
+//     var notification = $(`
+//         <div class="notification ${type}">
+//             <span>${message}</span>
+//             <button class="close-btn">&times;</button>
+//         </div>
+//     `);
 
-    //Mostrar la notificación con animacion
-    setTimeout(function (){
-        notification.addClass('show');
-    }, 10);
+//     $('#notification-container').append(notification);
 
-    //Cerrar manualmente
-    notification.find('.close-btn').on('click', function (){
-        notification.removeClass('show');
-        setTimeout(function (){
-            notification.remove();
-        }, 500);
-    });
+//     //Mostrar la notificación con animacion
+//     setTimeout(function (){
+//         notification.addClass('show');
+//     }, 10);
 
-    //Eliminar automáticamente despues de 5 segundos
-    setTimeout(function ()
-    {
-        notification.removeClass('show');
-        setTimeout(function (){
-            notification.remove();
-        }, 500);
-    }, 5000);
-}
+//     //Cerrar manualmente
+//     notification.find('.close-btn').on('click', function (){
+//         notification.removeClass('show');
+//         setTimeout(function (){
+//             notification.remove();
+//         }, 500);
+//     });
+
+//     //Eliminar automáticamente despues de 5 segundos
+//     setTimeout(function ()
+//     {
+//         notification.removeClass('show');
+//         setTimeout(function (){
+//             notification.remove();
+//         }, 500);
+//     }, 5000);
+// }
 
 //Funcion para eliminar los productos del carrito
 
