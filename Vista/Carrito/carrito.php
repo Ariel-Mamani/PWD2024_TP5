132138dfa8a72244d2b6f2948b7d3262cd9bf986
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2024-11-21 10:28:07 -0300

    Update carrito.php
    Anda el carrito

--- Vista/Carrito/carrito.php
+++ Vista/Carrito/carrito.php
@@ -1,9 +1,10 @@
 <?php
 $titulo = "Carrito"; 
 include_once '../Estructura/header.php';
-//$objSession = new Session();
 $user = $objSession->getUsuario()->getusnombre();
 $correo = $objSession->getUsuario()->getusmail();
+$objAbmCompra = new AbmCompra();
+$carrito = $objAbmCompra->mostrarCompra();
 
 
 //Hay que verificar si el que ingresa al carrito es un usuario registrado
@@ -14,19 +15,49 @@ echo $_SESSION['idcompra'];
 echo $_SESSION['idusuario'];
 ?>
 
-
 <form action="" method="post">
-
     <b>Usuario: </b><?php echo $user; ?><br>
     <b>Correo: </b><?php echo $correo; ?><br>
     <b>Fecha: </b> <?php echo date('d-m-Y'); ?><br><br>
-
     <div class="container">
         <div class="table-responsive">
-          <table id="tabla1">
-        
-
-          </table>
+            <table class="table table-bordered" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Artículo</th>
+                        <th>Cant</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (is_array($carrito) && count($carrito) > 0): ?>
+                        <?php
+                            $totalGeneral = 0;
+                            foreach ($carrito as $index => $item) :
+                            $totalItem = $item['cicantidad'] * $item['proprecio'];
+                            $totalGeneral += $totalItem;
+                         ?>
+                        <tr>
+                            <td><?php echo $index + 1; ?></td>
+                            <td><?php echo htmlspecialchars($item['pronombre']); ?></td>
+                            <td><?php echo htmlspecialchars($item['cicantidad']); ?></td>
+                            <td><?php echo '$' . htmlspecialchars($item['proprecio']); ?></td>
+                            <td>
+                                <button class="btn btn-danger eliminar-carrito" data-index="<?php echo $index; ?>" data-id='<?php echo $item['idproducto'];?>'><i class="bi bi-trash-fill"></i></button>
+                                <button><i class="bi bi-caret-up-fill"></i></button>
+                                <button><i class="bi bi-caret-down-fill"></i></button>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                        <?php else: ?>
+                        <tr>
+                            <td colspan="5">El carrito está vacío.</td>
+                        </tr>
+                        <?php endif; ?>
+                    </tbody>
+            </table><br>
             <h4>
                 Total: <?php echo '$' . number_format(isset($totalGeneral) ? $totalGeneral : 0, 2); ?>
             </h4> <!-- Muestra el total -->
@@ -60,20 +91,4 @@ $(document).on('click', '.eliminar-carrito', function() {
         }
     });
 });
-
-$('#tabla1').datagrid({
-        width: 710,
-        heigth: 300,
-        fitColumns: true,
-        singleSelect: true,
-        striped: true,
-    url:'carrito_list.php',
-    columns:[[
-        {field:'idproducto', title:'Id Producto', width:100, align:'center'},
-        {field:'pronombre', title:'Nombre', width:300, align:'center'},
-        {field:'cicantidad', title:'Cantidad', width:100, align:'center'},
-        {field:'proprecio', title:'Precio', width:100, align:'center'},
-    ]]
-});
 </script>
-
