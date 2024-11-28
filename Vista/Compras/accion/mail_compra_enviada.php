a040402f7609bc671cc6ee5c2e2419b08ae23f40
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2024-11-28 12:13:35 -0300

    Detalle

--- Vista/Compras/accion/mail_compra_enviada.php
+++ Vista/Compras/accion/mail_compra_enviada.php
@@ -4,13 +4,13 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Random\Engine\Secure;
 
-// Incluir el autoloader de Composer
+//Incluir el autoloader de Composer
 require '../../../vendor/autoload.php';
 include_once '../../../configuracion.php';
 
 error_log("Inicio del script mail_compra_enviada.php");
 
-// Obtener la sesión y la información del usuario
+//Obtener la sesión y la información del usuario
 $objSession = new Session();
 error_log("Se instanció Session");
 
@@ -19,7 +19,7 @@ $correo = null;
 $idcompra = null;
 
 try {
-    //$user = $data['usnombre'];
+    $user = $objSession->getUsuario()->getusmail();
     $correo = $objSession->getUsuario()->getusmail();
     $idcompra = $data['idcompra'];
     error_log("Usuario obtenido: $user, Correo: $correo, ID Compra: $idcompra");
@@ -29,33 +29,33 @@ try {
 
 $objAbmCompra = new AbmCompra();
 
-// Configurar el correo
+//Configurar el correo
 $mail = new PHPMailer(true);
 try {
     error_log("Inicio de configuración de PHPMailer");
 
-    // Configuración del servidor SMTP
+    //Configuración del servidor SMTP
     $mail->isSMTP();
     $mail->Host = 'smtp.mail.yahoo.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'dariofuentealba@yahoo.com.ar';
-    $mail->Password = 'sxdxmrvdmewzhrnj';
+    $mail->Password = 'ljrkijgzmcjfgfrs';
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
     error_log("Configuración SMTP completada");
 
-    // Remitente
+    //Remitente
     $mail->setFrom('dariofuentealba@yahoo.com.ar', 'Pelitos');
     $mail->addAddress($correo, $user); // El correo del usuario
     error_log("Destinatario configurado: $correo ($user)");
 
-    // Contenido del correo
+    //Contenido del correo
     $mail->CharSet = 'UTF-8';  // Establecer la codificación a UTF-8, sino sale todo feo
     $mail->isHTML(true);
     $mail->Subject = 'Confirmación de Envío';
 
-    // Crear el cuerpo del correo con los detalles de la compra
-    $body = "<h2>¡Hola!</h2>";
+    //Crear el cuerpo del correo con los detalles de la compra
+    $body = "<h2>¡Hola, $user!</h2>";
     $body .= "<p>Su compra ha sido enviada.</p>";
     $body .= "<p>Gracias por tu compra. ¡Esperamos verte pronto!</p>";
     error_log("Cuerpo del correo creado");
